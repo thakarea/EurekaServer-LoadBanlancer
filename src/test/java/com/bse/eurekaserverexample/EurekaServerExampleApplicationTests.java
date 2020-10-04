985489b0176b1e8d95ed35a3f07d2fbaf9af5bd3
thakarea <akash.thakarea@gmail.com>
2020-10-04 23:36:52 +0530

    Eureka -server -load balancer

---
+++ src/test/java/com/bse/eurekaserverexample/EurekaServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bse.eurekaserverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
